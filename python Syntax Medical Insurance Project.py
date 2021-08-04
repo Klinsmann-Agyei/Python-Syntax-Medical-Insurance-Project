a2edd01dcfd85f764ad6f71389c39b3db83ab423
Klinsmann-Agyei <87317341+Klinsmann-Agyei@users.noreply.github.com>
2021-08-04 20:19:49 +0000

    Create python Syntax Medical Insurance Project.py
    Committing Python Syntax Project

---
+++ python Syntax Medical Insurance Project.py
@@ -0,0 +1,31 @@
+
+# create the initial variables below
+age = 28
+sex = 0
+bmi = 26.2
+num_of_children = 3
+smoker = 0
+
+# Add insurance estimate formula below
+insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+print("This person's insurance cost is "+ str(insurance_cost) + " dollars")
+
+# Age Factor
+age += 4
+new_insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+change_in_insurance = new_insurance_cost - insurance_cost
+print("The change in cost of insurance after increasing the age by 4 years is "+str(change_in_insurance)+"  dollars.")
+# BMI Factor
+age = 28
+bmi += 3.1
+new_insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+change_in_insurance = new_insurance_cost - insurance_cost
+print("The change in estimated insurance cost after increasing BMI by 3.1 is "+ str(change_in_insurance)+ "dollars.")
+# Male vs. Female Factor
+sex = 1
+bmi = 26.2
+sex = 1
+new_insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+change_in_insurance = new_insurance_cost - insurance_cost
+print("The change in estimated cost for being male instead of female is "+str(change_in_insurance)+ "dollars.")
+# Extra Practice
